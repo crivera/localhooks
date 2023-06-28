e0d52c039ab9599c67c79a7cd67684d3971518b0
Christopher Rivera <chris.rivera@taxfyle.com>
2023-06-28 09:58:57 -0400

    one more

--- apps/cli/src/cli.ts
+++ apps/cli/src/cli.ts
@@ -4,8 +4,16 @@ import { Command } from "commander";
 import axios from "axios";
 import EventSource from "eventsource";
 import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 
-const file = fs.readFileSync("./package.json", "utf-8");
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const file = fs.readFileSync(
+  path.resolve(__dirname, "../package.json"),
+  "utf-8"
+);
 
 const program = new Command();
 program
